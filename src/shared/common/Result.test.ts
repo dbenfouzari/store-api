2774eee803841c10fd90c0044da803383841d2e7
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2023-12-04 17:50:12 +0100

    Improve `Result` utility

--- src/shared/common/Result.test.ts
+++ src/shared/common/Result.test.ts
@@ -1,65 +1,150 @@
 import { Result } from "@shared/common/Result";
+import { ResultCannotGetErrorOfSuccess } from "@shared/domain/errors/ResultCannotGetErrorOfSuccess";
+import { ResultCannotGetValueOfFailure } from "@shared/domain/errors/ResultCannotGetValueOfFailure";
 
-describe("result", () => {
-  describe("with success value", () => {
-    const result = Result.ok("Hello");
+describe("Result", () => {
+  describe("`ok` constructor", () => {
+    const resultOk = Result.ok("ok");
 
-    it("should have a `isSuccess` property set to true", () => {
-      expect(result.isSuccess).toBe(true);
+    it("should have `isFailure` property set to false", () => {
+      expect(resultOk.isFailure).toBe(false);
     });
 
-    it("should have a `isFailure` property set to false", () => {
-      expect(result.isFailure).toBe(false);
+    it("should have `isSuccess` property set to true", () => {
+      expect(resultOk.isSuccess).toBe(true);
     });
 
-    it("should return value", () => {
-      expect(result.getValue()).toBe("Hello");
+    it("should have `getValue` method returning the value", () => {
+      expect(resultOk.value).toBe("ok");
     });
 
-    it("should return an empty error", () => {
-      expect(result.getErrorValue()).toBe("");
-    });
-  });
-
-  describe("with error value", () => {
-    const result = Result.fail("Oh no!");
-
-    it("should have a `isSuccess` property set to false", () => {
-      expect(result.isSuccess).toBe(false);
+    it("should throw an error when calling `getError` method", () => {
+      expect(() => resultOk.error).toThrow(ResultCannotGetErrorOfSuccess);
     });
 
-    it("should have a `isFailure` property set to true", () => {
-      expect(result.isFailure).toBe(true);
-    });
-
-    it("should throw an error if we try to get its value", () => {
-      expect(() => {
-        result.getValue();
-      }).toThrow("Can't get the value of an error result. Use `getErrorValue` instead.");
-    });
-
-    it("should return an error", () => {
-      expect(result.getErrorValue()).toBe("Oh no!");
+    it("should have `match` method returning the value", () => {
+      expect(
+        resultOk.match(
+          () => "error",
+          (value) => value
+        )
+      ).toBe("ok");
     });
   });
 
-  describe("with child inheritance that allows a public constructor", () => {
-    class ChildResult<T> extends Result<T> {
-      public constructor(isSuccess: boolean, error?: string, value?: T) {
-        super(isSuccess, error, value);
-      }
-    }
+  describe("`fail` constructor", () => {
+    const resultFail = Result.fail("fail");
 
-    it("should protect against defining isSuccess and an error message in the same time", () => {
-      expect(() => {
-        new ChildResult(true, "Error", "Value");
-      }).toThrow("InvalidOperation: A result cannot be successful and contain an error.");
+    it("should have `isFailure` property set to true", () => {
+      expect(resultFail.isFailure).toBe(true);
     });
 
-    it("should protect against defining isFailure without an error", () => {
-      expect(() => {
-        new ChildResult(false, undefined, "Value");
-      }).toThrow("InvalidOperation: A failing result needs to contain an error message.");
+    it("should have `isSuccess` property set to false", () => {
+      expect(resultFail.isSuccess).toBe(false);
+    });
+
+    it("should have `getError` method returning the error", () => {
+      expect(resultFail.error).toBe("fail");
+    });
+
+    it("should throw an error when calling `getValue` method", () => {
+      expect(() => resultFail.value).toThrow(ResultCannotGetValueOfFailure);
+    });
+
+    it("should have `match` method returning the error", () => {
+      expect(
+        resultFail.match(
+          (error) => error,
+          () => "ok"
+        )
+      ).toBe("fail");
+    });
+  });
+
+  describe("`combine` method", () => {
+    const resultOk = Result.ok("ok");
+    const resultFail = Result.fail("fail");
+
+    it("should return a success result when all results are success", () => {
+      expect(Result.combine(resultOk, resultOk).isSuccess).toBe(true);
+    });
+
+    it("should return a failure result when one result is a failure", () => {
+      expect(Result.combine(resultOk, resultFail).isFailure).toBe(true);
+    });
+
+    it("should return a failure result when all results are failures", () => {
+      expect(Result.combine(resultFail, resultFail).isFailure).toBe(true);
+    });
+  });
+
+  describe("`match` method", () => {
+    const resultOk = Result.ok("ok");
+    const resultFail = Result.fail("fail");
+
+    let successCallback: jest.Mock;
+    let failureCallback: jest.Mock;
+
+    beforeEach(() => {
+      successCallback = jest.fn().mockImplementation((value) => value);
+      failureCallback = jest.fn().mockImplementation((error) => error);
+    });
+
+    it("should execute the `onSuccess` callback when the result is a success", () => {
+      expect(resultOk.match(failureCallback, successCallback)).toBe("ok");
+    });
+
+    it("should call the `onSuccess` callback with the value", () => {
+      resultOk.match(failureCallback, successCallback);
+
+      expect(successCallback).toHaveBeenCalledWith("ok");
+    });
+
+    it("should execute the `onFailure` callback when the result is a failure", () => {
+      expect(resultFail.match(failureCallback, successCallback)).toBe("fail");
+    });
+
+    it("should call the `onFailure` callback with the error", () => {
+      resultFail.match(failureCallback, successCallback);
+
+      expect(failureCallback).toHaveBeenCalledWith("fail");
+    });
+  });
+
+  describe("`matchObj` method", () => {
+    const resultOk = Result.ok("ok");
+    const resultFail = Result.fail("fail");
+
+    let successCallback: jest.Mock;
+    let failureCallback: jest.Mock;
+
+    beforeEach(() => {
+      successCallback = jest.fn().mockImplementation((value) => value);
+      failureCallback = jest.fn().mockImplementation((error) => error);
+    });
+
+    it("should execute the `onSuccess` callback when the result is a success", () => {
+      expect(resultOk.matchObj({ fail: failureCallback, ok: successCallback })).toBe(
+        "ok"
+      );
+    });
+
+    it("should call the `onSuccess` callback with the value", () => {
+      resultOk.matchObj({ fail: failureCallback, ok: successCallback });
+
+      expect(successCallback).toHaveBeenCalledWith("ok");
+    });
+
+    it("should execute the `onFailure` callback when the result is a failure", () => {
+      expect(resultFail.matchObj({ fail: failureCallback, ok: successCallback })).toBe(
+        "fail"
+      );
+    });
+
+    it("should call the `onFailure` callback with the error", () => {
+      resultFail.matchObj({ fail: failureCallback, ok: successCallback });
+
+      expect(failureCallback).toHaveBeenCalledWith("fail");
     });
   });
 });
--- src/shared/common/Result.ts
+++ src/shared/common/Result.ts
@@ -1,60 +1,130 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ResultCannotGetErrorOfSuccess } from "@shared/domain/errors/ResultCannotGetErrorOfSuccess";
+import { ResultCannotGetValueOfFailure } from "@shared/domain/errors/ResultCannotGetValueOfFailure";
 
-export class Result<T> {
-  public readonly isSuccess: boolean;
-  public readonly isFailure: boolean;
-  private readonly error: string;
+export class Result<T, E> {
+  /** Stores the value of the result when it's successful. */
   private readonly _value: T;
+  /** Stores the error of the result when it's a failure. */
+  private readonly _error: E | undefined;
 
-  protected constructor(isSuccess: boolean, error: string = "", value?: T) {
-    if (isSuccess && error) {
-      throw new Error(
-        "InvalidOperation: A result cannot be successful and contain an error."
-      );
-    }
-    if (!isSuccess && !error) {
-      throw new Error(
-        "InvalidOperation: A failing result needs to contain an error message."
-      );
-    }
+  /** Indicates whether the result is successful or not. */
+  public readonly isSuccess: boolean;
+  /** Indicates whether the result is a failure or not. */
+  public readonly isFailure: boolean;
 
-    this.isSuccess = isSuccess;
-    this.isFailure = !isSuccess;
-    this.error = error;
-    this._value = value as T;
+  //#region Constructors
+  /**
+   * Creates a new Result.
+   * @param value The value of the result.
+   * @param error The error of the result.
+   * @returns A new successful result.
+   */
+  private constructor(value: T, error?: E) {
+    this._value = value;
+    this._error = error;
 
-    Object.freeze(this);
+    this.isSuccess = !error;
+    this.isFailure = !!error;
   }
 
-  public getValue(): T {
-    if (this.isFailure) {
-      throw new Error(
-        "Can't get the value of an error result. Use `getErrorValue` instead."
-      );
-    }
-
-    return this._value;
+  /**
+   * Creates a new successful result.
+   * @param value The value of the result.
+   * @returns A new successful result.
+   */
+  public static ok<U, E>(value: U): Result<U, E> {
+    return new Result<U, E>(value);
   }
 
-  public getErrorValue() {
-    return this.error;
+  /**
+   * Creates a new failure result.
+   * @param error The error of the result.
+   * @returns A new failure result.
+   */
+  public static fail<E>(error: E): Result<unknown, E> {
+    return new Result<unknown, E>(undefined, error);
   }
+  //#endregion
 
-  public static ok<U>(value?: U): Result<U> {
-    return new Result<U>(true, "", value);
-  }
-
-  public static fail<U>(error: string) {
-    return new Result<U>(false, error);
-  }
-
-  public static combine(...results: Result<any>[]) {
+  //#region Static methods
+  /**
+   * Combines multiple results into one.
+   * @param results The results to be combined.
+   * @returns A new result.
+   */
+  public static combine(...results: Result<any, any>[]): Result<null, any> {
     for (const result of results) {
       if (result.isFailure) {
         return result;
       }
     }
 
-    return Result.ok<any>();
+    return Result.ok(null);
   }
+  //#endregion
+
+  //#region Getters
+  /**
+   * Returns the value of the result.
+   * @returns The value of the result.
+   * @throws ResultCannotGetValueOfFailure An error if the result is a failure.
+   */
+  public get value(): T {
+    if (this.isFailure) {
+      throw new ResultCannotGetValueOfFailure();
+    }
+
+    return this._value;
+  }
+
+  /**
+   * Returns the error of the result.
+   * @returns The error of the result.
+   * @throws ResultCannotGetErrorOfSuccess An error if the result is a success.
+   */
+  public get error(): E {
+    if (this.isFailure && this._error) {
+      return this._error;
+    }
+
+    throw new ResultCannotGetErrorOfSuccess();
+  }
+  //#endregion
+
+  //#region Public methods
+  /**
+   * Matches the result with the given functions.
+   * @param onFailure Callback to be executed when the result is a failure. Receives the error as a parameter.
+   * @param onSuccess Callback to be executed when the result is a success. Receives the value as a parameter.
+   * @returns The result of the executed callback.
+   */
+  public match<R>(onFailure: (error: E) => R, onSuccess: (value: T) => R): R {
+    if (this.isFailure) {
+      return onFailure(this._error as E);
+    }
+
+    return onSuccess(this._value);
+  }
+
+  /**
+   * Matches the result with the given functions.
+   * @param obj Object containing the callbacks to be executed.
+   * @param obj.fail Callback to be executed when the result is a failure. Receives the error as a parameter.
+   * @param obj.ok Callback to be executed when the result is a success. Receives the value as a parameter.
+   * @returns The result of the executed callback.
+   */
+  public matchObj<R>({
+    fail,
+    ok,
+  }: {
+    fail?: (error: E) => R;
+    ok?: (value: T) => R;
+  }): R | undefined {
+    if (this.isFailure) {
+      return fail?.(this._error as E);
+    }
+
+    return ok?.(this._value);
+  }
+  //#endregion
 }
